9e28d633651f8bd167f206348f13bfee7a5b435e
Pavan-Red-E <pavanreddykandula56@gmail.com>
2019-11-15 13:06:18 +0530

    first commi1

---
+++ sub.py
@@ -0,0 +1,7 @@
+number1 = input("First number: ") 
+number2 = input("\nSecond number: ") 
+  
+ 
+result = float(number1) - float(number2) 
+
+print("The result of {0} and {1} is {2}" .format(number1, number2, result))
